fdfb60983d57486416afa4a35f6f8662566b1676
Eli <eliogilvy98@gmail.com>
2024-04-01 21:45:49 -0500

    formatting

--- lua/lucifer/plugins/lsp.lua
+++ lua/lucifer/plugins/lsp.lua
@@ -9,10 +9,10 @@ return {
     config = function()
         require('mason').setup()
         require('mason-lspconfig').setup({
-            ensure_installed = {'lua_ls'},
+            ensure_installed = { 'lua_ls' },
             handlers = {
                 function(server)
-                    require('lspconfig')[server].setup{}
+                    require('lspconfig')[server].setup {}
                 end
             }
         })
