8f34bc888f672a2669af3d9a462b11a25b2927c9
Eli <eliogilvy98@gmail.com>
2024-04-05 22:51:55 -0500

    Setting up flutter tools

--- lua/lucifer/plugins/fluttertools.lua
+++ lua/lucifer/plugins/fluttertools.lua
@@ -5,5 +5,34 @@ return {
         'plenary',
         'dressing',
     },
-    config = true,
+    config = function()
+        require('flutter-tools').setup()
+    end,
+    keys = {
+        {
+            '<leader>rr',
+            '<cmd>FlutterRun<cr>',
+            desc = 'Flutter run'
+        },
+        {
+            '<leader>rd',
+            '<cmd>FlutterDevices<cr>',
+            desc = 'Flutter devices'
+        },
+        {
+            '<leader>re',
+            '<cmd>FlutterEmulators<cr>',
+            desc = 'Flutter emulators'
+        },
+        {
+            '<leader>ro',
+            '<cmd>FlutterOutlineToggle<cr>',
+            desc = 'Flutter toggle outline'
+        },
+        {
+            '<leader>rl',
+            '<cmd>FlutterReanalyze<cr>',
+            desc = 'Flutter reload lsp'
+        },
+    }
 }
