ed94933bea4aa38f0cf4595e245cec1a5dcabd9c
Eli <eliogilvy98@gmail.com>
2024-04-07 16:36:21 -0500

    More stuff
    - Added custom configs file to pull and place into project
    - Updated flutter tools configs

--- lua/lucifer/plugins/fluttertools.lua
+++ lua/lucifer/plugins/fluttertools.lua
@@ -7,6 +7,13 @@ return {
     },
     config = function()
         require('flutter-tools').setup{
+            decorations = {
+                statusline = {
+                    app_version = true,
+                    device = true,
+                    project_config = true
+                }
+            },
             lsp = {
                 color = {
                     enabled = true
