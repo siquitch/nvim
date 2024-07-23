da1f84704430835eb81c58ef3b735da2b4cb45b6
Eli <eliogilvy98@gmail.com>
2024-07-22 20:45:04 -0500

    Oil updates

--- lua/lucifer/plugins/oil.lua
+++ lua/lucifer/plugins/oil.lua
@@ -1,9 +1,14 @@
 return {
-    'stevearc/oil.nvim',
-    opts = {},
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require('oil').setup()
-        vim.keymap.set('n', '<leader>o', '<cmd>Oil<CR>', { desc = 'Toggle oil' })
-    end
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			prompt_save_on_select_new_entry = false,
+			keymaps = {
+				["<C-h"] = false,
+				["<C-s"] = false,
+			},
+		})
+		vim.keymap.set("n", "<leader>o", "<cmd>Oil<CR>", { desc = "Toggle oil" })
+	end,
 }
