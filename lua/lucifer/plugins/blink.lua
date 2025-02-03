91ebb81f11c921cce7c9b5625416ba78f0f9fd00
eliogilvy <eliogilvy98@gmail.com>
2025-02-03 08:34:04 -0600

    Cleanup

--- lua/lucifer/plugins/blink.lua
+++ lua/lucifer/plugins/blink.lua
@@ -8,7 +8,7 @@ return {
 			["<Down>"] = { "select_next", "fallback" },
 			["<Up>"] = { "select_prev", "fallback" },
 			["<C-k>"] = { "show_documentation", "hide_documentation" },
-			["<C-j>"] = { "show", "hide" },
+			["<C-space>"] = { "show", "hide" },
 			["<C-y>"] = { "accept", "fallback" },
 		},
 		appearance = {
