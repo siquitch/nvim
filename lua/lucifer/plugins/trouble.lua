580b0de447ed2136a21ee3b6c556736cb655d8a8
eliogilvy <eliogilvy98@gmail.com>
2025-02-23 12:35:02 -0600

    format

--- lua/lucifer/plugins/trouble.lua
+++ lua/lucifer/plugins/trouble.lua
@@ -1,25 +1,25 @@
 return {
-    'folke/trouble.nvim',
-    dependencies = { 'icons' },
-    name = 'trouble',
-    keys = {
-        {
-            '<leader>tw',
-            '<cmd>Trouble diagnostics toggle<cr>',
-            desc = 'Trouble toggle workspace'
-        },
-        {
-            '<leader>tr',
-            '<cmd>TroubleRefresh<cr>',
-            desc = 'Trouble refresh'
-        },
-    },
-    opts = {
-        win = {
-            position = "right",
-            size = {
-                width = 50
-            }
-        }
-    },
+	"folke/trouble.nvim",
+	dependencies = { "icons" },
+	name = "trouble",
+	keys = {
+		{
+			"<leader>tw",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Trouble toggle workspace",
+		},
+		{
+			"<leader>tr",
+			"<cmd>TroubleRefresh<cr>",
+			desc = "Trouble refresh",
+		},
+	},
+	opts = {
+		win = {
+			position = "right",
+			size = {
+				width = 50,
+			},
+		},
+	},
 }
