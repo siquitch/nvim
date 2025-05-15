c96765c7c9068ef7e980ef14c0f0df18fe592fa4
eliogilvy <eliogilvy98@gmail.com>
2025-05-15 09:46:33 -0500

    Lsp update

--- lua/lucifer/plugins/lsp.lua
+++ lua/lucifer/plugins/lsp.lua
@@ -1,6 +1,38 @@
 local servers = { "lua_ls", "clangd", "marksman", "gopls", "arduino_language_server" }
 local map = vim.keymap
 
+-- vim.lsp.config(
+-- 	"arduino_language_server",
+--     {cmd = {
+-- 		"arduino-language-server",
+-- 		"-cli-config",
+-- 		"C:\\Users\\Eli\\AppData\\Local\\Arduino15\\arduino-cli.yaml",
+-- 		"-fqbn",
+-- 		"arduino:avr:uno",
+-- 	},
+-- })
+--
+
+vim.lsp.config("dartls", {
+	init_options = {
+		onlyAnalyzeProjectsWithOpenFiles = true,
+		suggestFromUnimportedLibraries = true,
+		closingLabels = false,
+		outline = false,
+		flutterOutline = false,
+	},
+	settings = {
+		dart = {
+			completeFunctionCalls = true,
+			showTodos = true,
+			renameFilesWithClasses = "always",
+			enableSnippets = true,
+			updateImportsOnRename = true,
+		},
+	},
+})
+vim.lsp.enable("dartls", true)
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
@@ -36,75 +68,16 @@ return {
 			marksman = {},
 			gopls = {},
 			dartls = {},
+			ts_ls = {},
+			eslint = {},
 		},
 	},
 
 	config = function(_, opts)
-		local lsp = require("lspconfig")
 		require("mason").setup()
 		require("mason-lspconfig").setup({
-			automatic_installation = true,
 			ensure_installed = servers,
-			handlers = {
-				["lua_ls"] = function()
-					lsp.lua_ls.setup({
-						settings = {
-							Lua = {
-								vim.api,
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-							diagnostics = {
-								disable = { "missing-fields" },
-							},
-						},
-					})
-				end,
-				["clangd"] = function()
-					lsp.clangd.setup({})
-				end,
-				["marksman"] = function()
-					lsp.marksman.setup({})
-				end,
-				["gopls"] = function()
-					lsp.gopls.setup({})
-				end,
-				["arduino_language_server"] = function()
-					lsp.arduino_language_server.setup({
-						cmd = {
-							"arduino-language-server",
-							"-cli-config",
-							"C:\\Users\\Eli\\AppData\\Local\\Arduino15\\arduino-cli.yaml",
-							"-fqbn",
-							"arduino:avr:uno",
-						},
-					})
-				end,
-			},
+			automatic_enable = true,
 		})
-		lsp.dartls.setup({
-			init_options = {
-				onlyAnalyzeProjectsWithOpenFiles = true,
-				suggestFromUnimportedLibraries = true,
-				closingLabels = false,
-				outline = false,
-				flutterOutline = false,
-			},
-			settings = {
-				dart = {
-					completeFunctionCalls = true,
-					showTodos = true,
-					renameFilesWithClasses = "always",
-					enableSnippets = true,
-					updateImportsOnRename = true,
-				},
-			},
-		})
-
-		for server, config in pairs(opts.servers) do
-			local capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-			lsp[server].setup({ capabilities = capabilities })
-		end
 	end,
 }
